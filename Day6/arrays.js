4f4d5bd0a231c9da268b4cfb19c6a1b8afdb2e0b
Saroj Dahal <isarojdahal@gmail.com>
2023-09-25 14:25:31 +0545

    add: Day 6 dir

---
+++ Day6/arrays.js
@@ -0,0 +1,5 @@
+let studentList1 = ["Tom", "Tyson", "Spidy", "Jack", "Smith"];
+
+console.log(studentList1.fill("rocket", 3, 5));
+
+console.log(studentList1);
---
+++ Day6/ds.js
@@ -0,0 +1,15 @@
+// let student = {
+//   name: "Ramesh",
+//   age: 10,
+// };
+
+// let student2 = {
+//   test: {
+//     ok: "ok",
+//   },
+//   name: "Thomas",
+//   hobbies: "cricket",
+// };
+
+// let c = Object.assign({ ...student2 }, student);
+// c;
