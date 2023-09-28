5d7f27d87a8d926be2c4caffd35c2312b2aed835
Saroj Dahal <isarojdahal@gmail.com>
2023-09-28 14:43:16 +0545

    add: Day9 dir

---
+++ Day9/file.js
@@ -0,0 +1,19 @@
+export const value = "VALUE";
+
+export function add(a, b) {
+  return a + b;
+}
+
+export let student = {
+  name: "Mahesh",
+  age: 20,
+};
+
+export default class Product {
+  constructor() {}
+  add() {
+    console.log("product added");
+  }
+}
+
+// 
---
+++ Day9/main.js
@@ -0,0 +1,45 @@
+// // import { value, add, student as studentObj } from "./a.js";
+
+// // console.log(value);
+// // console.log(add(2, 3));
+// // console.log(studentObj);
+
+import Product from "./js/a.js";
+
+let i = new Product();
+i.add();
+
+// console.warn("12");
+// console.error("error msg");
+
+// console.info("hello");
+
+// const productList = [
+//   {
+//     name: "Pen Bucket",
+//     price: 120,
+//   },
+//   {
+//     name: "Watch",
+//     price: "9000",
+//   },
+// ];
+
+// console.table(productList);
+
+//
+// console.log("sdf");
+
+// console.group("For Loop data");
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// console.groupEnd();
+
+// console.log("adsfsdf");
+
+// console.time("timer");
+// for (let i = 0; i < 1000; i++) {
+//   console.log("i", i);
+// }
+// console.timeEnd("timer");
