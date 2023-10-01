337e8d1f774ab4e441c85e1391e8a801051e8c81
Saroj Dahal <isarojdahal@gmail.com>
2023-10-01 19:49:29 +0545

    add: Day12 dir

---
+++ Day12/chaining.js
@@ -0,0 +1,30 @@
+let peopleList = [
+  {
+    name: "Kanchan",
+    age: 20,
+    gender: "F",
+  },
+  {
+    name: "Kanchana",
+    age: 14,
+    gender: "F",
+  },
+  {
+    name: "Akshay",
+    age: 30,
+    gender: "M",
+  },
+];
+
+// Chaining.
+// let result = peopleList
+//   .filter((people) => people.age >= 18)
+//   .sort((a, b) => b.age - a.age)
+//   .map((v) => {
+//     document.body.innerHTML += `<li>${v.name} is ${v.age} years old. So, ${
+//       v.gender == "F" ? "she" : "he"
+//     } can vote
+//     </li>`;
+//   });
+
+result;
