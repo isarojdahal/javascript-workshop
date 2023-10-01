83859be40f927cc4e253ed721fee4f3cddcdec90
Saroj Dahal <isarojdahal@gmail.com>
2023-10-01 10:50:53 +0545

    add : Day10 and Day11 dir

---
+++ Day10/script.js
@@ -0,0 +1,20 @@
+// const div = document.getElementById("main-div");
+
+// // div.classList.add("divstyle");
+// // div.classList.remove("divstyle");
+
+// // div.classList.toggle("main", false);
+
+// var language = {
+//   title: {
+//     en: "Title",
+//     np: "shrishak",
+//   },
+// };
+
+// // document.getElementById
+// // document.getElementsByTagName
+// // document.getElementsByClassName
+// // document.querySelector();
+// // document.querySelectorAll();
+// // document.children;
